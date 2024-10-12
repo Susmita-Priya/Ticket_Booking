23bb0741d0361f75c56b9d323ab4e991c00c40cd
MD ANIKUL ISLAM <anikul.islam01@northsouth.edu>
2024-10-12 15:43:13 +0600

    update on type and cat

---
+++ resources/views/admin/pages/type/index.blade.php
@@ -0,0 +1,149 @@
+@extends('admin.app')
+@section('admin_content')
+    {{-- CKEditor CDN --}}
+    <div class="row">
+        <div class="col-12">
+            <div class="page-title-box">
+                <div class="page-title-right">
+                    <ol class="breadcrumb m-0">
+                        <li class="breadcrumb-item"><a href="javascript: void(0);">Online Ticket Booking</a></li>
+                        <li class="breadcrumb-item"><a href="javascript: void(0);">Resource</a></li>
+                        <li class="breadcrumb-item active">Type!</li>
+                    </ol>
+                </div>
+                <h4 class="page-title">Type!</h4>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <div class="d-flex justify-content-end">
+                    @can('type-create')
+                        <button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#addNewModalId">Add New</button>
+                    @endcan
+                </div>
+            </div>
+            <div class="card-body">
+                <table id="basic-datatable" class="table table-striped dt-responsive nowrap w-100">
+                    <thead>
+                    <tr>
+                        <th>S/N</th>
+                        <th>Name</th>
+                        <th>Status</th>
+                        <th>Action</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($type as $key=>$typeData)
+                        <tr>
+                            <td>{{++$key}}</td>
+                            <td>{{$typeData->title}}</td>
+                            <td>{!! Str::limit($typeData->details, 30) !!}</td>
+                            <td>{{$typeData->status==1? 'Active':'Inactive'}}</td>
+                            <td style="width: 100px;">
+                                <div class="d-flex justify-content-end gap-1">
+                                    @can('type-edit')
+                                        <button type="button" class="btn btn-info btn-sm" data-bs-toggle="modal" data-bs-target="#editNewModalId{{$typeData->id}}">Edit</button>
+                                    @endcan
+                                    @can('type-delete')
+                                        <a href="{{route('type.destroy',$typeData->id)}}"class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#danger-header-modal{{$typeData->id}}">Delete</a>
+                                    @endcan
+                                </div>
+                            </td>
+                            <!--Edit Modal -->
+                            <div class="modal fade" id="editNewModalId{{$typeData->id}}" data-bs-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="editNewModalLabel{{$typeData->id}}" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h4 class="modal-title" id="addNewModalLabel{{$typeData->id}}">Edit</h4>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <form method="post" action="{{route('type.update',$typeData->id)}}" enctype="multipart/form-data">
+                                                @csrf
+                                                @method('PUT')
+                                                <div class="row">
+                                                    <div class="col-12">
+                                                        <div class="mb-3">
+                                                            <label for="name" class="form-label">Name</label>
+                                                            <input type="text" id="name" name="name" value="{{$typeData->form}}"
+                                                                   class="form-control" placeholder="Enter name" required>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col-12">
+                                                        <div class="mb-3">
+                                                            <label for="example-select" class="form-label">Status</label>
+                                                            <select name="status" class="form-select">
+                                                                <option value="1" {{ $typeData->status === 1 ? 'selected' : '' }}>Active</option>
+                                                                <option value="0" {{ $typeData->status === 0 ? 'selected' : '' }}>Inactive</option>
+                                                            </select>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="d-flex justify-content-end">
+                                                    <button class="btn btn-primary" type="submit">Update</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Delete Modal -->
+                            <div id="danger-header-modal{{$typeData->id}}" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="danger-header-modalLabel{{$typeData->id}}" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered">
+                                    <div class="modal-content">
+                                        <div class="modal-header modal-colored-header bg-danger">
+                                            <h4 class="modal-title" id="danger-header-modalLabe{{$typeData->id}}l">Delete</h4>
+                                            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <h5 class="mt-0">Are You Went to Delete this ? </h5>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+                                            <a href="{{route('type.destroy',$typeData->id)}}" class="btn btn-danger">Delete</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <!--Add Modal -->
+    <div class="modal fade" id="addNewModalId" data-bs-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="addNewModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="addNewModalLabel">Add</h4>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form method="post" action="{{route('type.store')}}">
+                        @csrf
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="mb-3">
+                                    <label for="name" class="form-label">Name</label>
+                                    <input type="text" id="name" name="name"
+                                           class="form-control" placeholder="Enter name" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-end">
+                            <button class="btn btn-primary" type="submit">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
