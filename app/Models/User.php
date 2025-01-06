83122ea12914412a4d2e22df622b56499b335693
Md-Anikul-Islam <anikul.islam01@northsouth.edu>
2025-01-06 10:45:12 +0600

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ use Spatie\Permission\Traits\HasRoles;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
-    
+
 
     /**
      * The attributes that are mass assignable.
@@ -26,6 +26,7 @@ class User extends Authenticatable
         'email_verified_at',
         'password',
         'verification_code',
+        'image',
         'status',
         'role'
     ];
